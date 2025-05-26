ee8edde471f9816d8b30967ef6f52c8352b7dc12
Dawit Firew <dawitfirew439@gmail.com>
2025-05-26 14:35:18 +0300

    Initial commit - Teacher interface

---
+++ frontend-javascript/0x04-TypeScript/task_1/dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+// ✅ Example usage
+const teacher3 = {
+    firstName: "John",
+    lastName: "Doe",
+    fullTimeEmployee: false,
+    location: "London",
+    contract: false, // Additional attribute allowed
+};
+console.log(teacher3);
